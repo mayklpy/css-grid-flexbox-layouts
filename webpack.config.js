90387fda98fae2e5f18e7c6cc0d91f8561d45c99
mchlrtnz <mchlrtnz@gmail.com>
2020-01-12 19:34:26 -0500

    update webpack config and configure /docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ let pages = fse
 class RunAfterCompile {
   apply(compiler) {
     compiler.hooks.done.tap("Copy images", function() {
-      fse.copySync("./app/assets/images", "./dist/assets/images");
+      fse.copySync("./app/assets/images", "./docs/assets/images");
     });
   }
 }
@@ -88,7 +88,7 @@ if (currentTask == "build") {
   config.output = {
     filename: "[name].[chunkhash].js",
     chunkFilename: "[name].[chunkhash].js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "docs")
   };
 
   config.mode = "production";
